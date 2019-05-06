6aa462a2c485cb491252b51e7da12ea9a782df14
michaelfahmy <michaelfahmy1@gmail.com>
2019-05-07 01:01:22 +0200

    Added testing and validations for airplane data model

--- spec/models/airplane_spec.rb
+++ spec/models/airplane_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Airplane, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    described_class.new(model: 'Airbus 777',
+                        business_class_rows: 6,
+                        economy_class_rows: 44,
+                        seats_per_row: 6)
+  end
+
+  describe 'Associations' do
+    it 'has many flights' do
+      association = described_class.reflect_on_association(:flights)
+      expect(association.macro).to eq :has_many
+    end
+  end
+
+  describe 'Validations' do
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without model' do
+      subject.model = nil
+      expect(subject).not_to be_valid
+    end
+
+    it 'is not valid without business class rows' do
+      subject.business_class_rows = nil
+      expect(subject).not_to be_valid
+    end
+
+    it 'is not valid without economy class rows' do
+      subject.economy_class_rows = nil
+      expect(subject).not_to be_valid
+    end
+
+    it 'is not valid without seats per row' do
+      subject.seats_per_row = nil
+      expect(subject).not_to be_valid
+    end
+  end
 end
