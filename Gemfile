75706298b5a870597594115a002fa248c819fe60
michaelfahmy <michaelfahmy1@gmail.com>
2019-05-07 03:25:35 +0200

    Updated seed file with airplanes and flights, added rubocop rules

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'airports'
 gem 'bootsnap', '>= 1.1.0', require: false
 
 group :development, :test do
+  gem 'faker'
   gem 'rspec-rails'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,39 @@
-if Rails.env.development?
-  User.create!(
-    first_name: 'Admin',
-    last_name: 'One',
-    role: 'manager',
-    email: 'admin1@uala.com',
-    password: 'admin1@uala.com',
-    password_confirmation: 'admin1@uala.com'
+User.create!(
+  first_name: 'John',
+  last_name: 'Doe',
+  role: 'manager',
+  email: 'admin@example.com',
+  password: 'admin@example.com',
+  password_confirmation: 'admin@example.com'
+)
+
+airplane_models = ['Airbus A321ceo', 'Boeing 787-8', 'Boeing 777-200ER', 'Russian A-50', 'Airbus A320neo', 'Airbus A350-900', 'B747-400', 'DC-9-10', 'DC-9-50']
+airplane_models.each do |model|
+  Airplane.create!(
+    model: model,
+    business_class_rows: rand(2..6),
+    economy_class_rows: rand(20..50),
+    seats_per_row: rand(4..9)
   )
+end
+
+Airplane.find_each do |airplane|
+  departure_time1 = Faker::Time.forward(10)
+  arrival_time1 = departure_time1 + rand(2..19).hours
+  departure_time2 = Faker::Time.between(11.days.from_now, 20.days.from_now)
+  arrival_time2 = departure_time2 + rand(2..19).hours
+
+  airplane.flights.create!([{
+    number: rand(100..999),
+    departure_airport: Airports.iata_codes[rand(0...Airports.iata_codes.length)],
+    destination_airport: Airports.iata_codes[rand(0...Airports.iata_codes.length)],
+    departure_time: departure_time1,
+    arrival_time: arrival_time1,
+  }, {
+    number: rand(100..999),
+    departure_airport: Airports.iata_codes[rand(0...Airports.iata_codes.length)],
+    destination_airport: Airports.iata_codes[rand(0...Airports.iata_codes.length)],
+    departure_time: departure_time2,
+    arrival_time: arrival_time2,
+  }])
 end
