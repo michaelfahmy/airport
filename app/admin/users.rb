fc4ee1c1588691220598105c9dd648f99349f64d
michaelfahmy <michaelfahmy1@gmail.com>
2019-05-08 04:57:03 +0200

    Added time zone to permitted params of user

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register User do
-  permit_params :first_name, :last_name, :role, :email, :password, :password_confirmation
+  permit_params :first_name, :last_name, :role, :time_zone, :email, :password, :password_confirmation
 
   scope :all
   scope :manager
