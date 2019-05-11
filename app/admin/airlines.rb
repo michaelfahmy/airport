83adde911fc625a73da7d23442228912733c9276
michaelfahmy <michaelfahmy1@gmail.com>
2019-05-11 21:59:54 +0200

    Updated airline filters for activeadmin

--- app/admin/airlines.rb
+++ app/admin/airlines.rb
@@ -1,6 +1,9 @@
 ActiveAdmin.register Airline do
   permit_params :name, :currency
 
+  filter :name_contains
+  filter :currency, as: :select, collection: Money::Currency.table.keys.map(&:upcase)
+
   form do |f|
     f.inputs do
       f.input :name
